28dc8ab08b5cbb6e423e337a4767607466d768fd
jadisonlopes <jadison.lopes.s@gmail.com>
2024-02-29 02:22:01 -0300

    chore: Remove código de teste.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
 import app from './app';
-import VendaContratoServico from './app/services/VendaContratoServico';
-
-const v = new VendaContratoServico();
-v.recuperar('152305').then(v => {console.log('Venda-recuperar', v)}).catch(c => {console.error(c)});
 
 app.listen(3030); 
